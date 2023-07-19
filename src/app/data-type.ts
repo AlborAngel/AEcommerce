9ec26d3c7e42a6d40b25a338bfcd369ae2549385
Angel <albor532@gmail.com>
2023-07-19 18:43:53 -0300

    Inicio del Proyecto

---
+++ src/app/data-type.ts
@@ -0,0 +1,17 @@
+export interface signUp{
+  name: string,
+  email: string,
+  password: string
+}
+export interface login{
+  email: string,
+  password: string
+}
+export interface product{
+  name: string,
+  price: number,
+  category: string,
+  color: string,
+  image: string,
+  description: string
+}
